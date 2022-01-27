6596177911770bc11ef03cfce03d0812cfc8c054
JoyH127 <hh495@scarletmail.rutgers.edu>
2022-01-27 00:04:40 -0500

    first commit

--- W1/D3/For-Loops.js
+++
@@ -1,8 +0,0 @@
-let countDown = 10;
-// ADD CODE HERE
-for (let i = 0; i < 10; i++) {
-  countDown--;
-  console.log(countDown);
-}
-// Uncomment the below line to check your work
-// console.log(countDown) // -> should print 0;
---
+++ ex1_10.js
@@ -0,0 +1,4 @@
+// ex 1  erase last element 400, 500
+
+var nums;
+ = [100,200,300,400,500];
