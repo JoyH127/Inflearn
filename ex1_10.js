b5d549ba0435ef6479dc47e3633de5237a5fbc93
JoyH127 <hh495@scarletmail.rutgers.edu>
2022-01-27 00:24:48 -0500

    complete first problem.

--- ex1_10.js
+++ ex1_10.js
@@ -1,8 +1,7 @@
 // ex 1  erase last element 400, 500
 
-var nums;
- = [100,200,300,400,500];
+var nums = [100, 200, 300, 400, 500];
 
- nums.pop();
- nums.pop();
- console.log(nums);
+nums.pop();
+nums.pop();
+console.log(nums);
